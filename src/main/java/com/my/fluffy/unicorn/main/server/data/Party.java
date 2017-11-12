a963ae542f20f166fbcf3eecb5d371ad0fa3abca
Tobias Beeh <qwertzuiop2@t-online.de>
2017-11-12 12:51:34 +0100

    improved java packages and fixed minor issues

---
+++ src/main/java/com/my/fluffy/unicorn/main/server/data/Party.java
@@ -0,0 +1,12 @@
+package com.my.fluffy.unicorn.main.server.data;
+
+public class Party {
+
+    public int id;
+    public String name;
+
+    public Party(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
