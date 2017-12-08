8527cba39a3e6eb50a777aa9898d4803f039a1ca
NameThatDoesNotExistAlready <monika.pichlmair@student.uni-augsburg.de>
2017-12-08 12:10:36 +0100

    minorchanges to remove materialized views

--- src/main/resources/sql/get-amount-per-gender.sql
+++ src/main/resources/sql/get-amount-per-gender.sql
@@ -1,3 +1,3 @@
 select c.sex, count(*) as total
-from election.parliamentmembers m join election.candidates c on m.id = c.id
+from election.parliamentmembers2017 m join election.candidates c on m.id = c.id
 group by c.sex
