ec4bbcc3e50ae54d8a9c305d43a779a6eb8ef8e4
Tobias Beeh <tobias.beeh@t-online.de>
2017-12-15 13:50:53 +0100

    changed tokens to alphanum

--- token-gen/src/main/java/Main.java
+++ token-gen/src/main/java/Main.java
@@ -6,7 +6,9 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         int numOfTokens = Integer.parseInt(args[0]);
-        int distinctPrefixCount = getPrefixCount(Integer.parseInt(args[1]));
+        int prefixLen = Integer.parseInt(args[1]);
+        int tokenLen = Integer.parseInt(args[2]);
+        int distinctPrefixCount = getPrefixCount(prefixLen);
 
         // distribute num of tokens on prefixes
         int[] count = new int[distinctPrefixCount];
@@ -17,15 +19,28 @@ public class Main {
             }
         }
 
-        TokenToDatabase.storeTokensInDatabase(IntStream.range(0, distinctPrefixCount).boxed().flatMap(i ->
-                distinctTokensWithPrefix(count[i], String.format("%02X", i))));
+        TokenToDatabase.storeTokensInDatabase(
+                IntStream.range(0, distinctPrefixCount)
+                        .boxed()
+                        .flatMap(i ->
+                                distinctTokensWithPrefix(count[i], tokenLen, getPrefix(i, prefixLen)).peek(System.out::println)));
     }
 
     private static int getPrefixCount(int prefixLen) {
-        return (int) Math.pow(16, prefixLen);
+        return (int) Math.pow(36, prefixLen);
     }
 
-    private static Stream<String> distinctTokensWithPrefix(int count, String prefix) {
-        return Stream.generate(() -> Token.genToken(12 - prefix.length())).distinct().limit(count).map(t -> prefix + t);
+    private static Stream<String> distinctTokensWithPrefix(int count, int length, String prefix) {
+        return Stream.generate(() -> Token.genAlphaNumToken(length - prefix.length())).distinct().limit(count).map(t -> prefix + t);
+    }
+
+    private static String getPrefix(long num, int len) {
+        char[] chars = new char[len];
+        for (int i = 0; i < len; i++) {
+            long val = Math.round(Math.pow(36, i));
+            int c = (int) (num / val) % 36;
+            chars[i] = Token.intToChar(c);
+        }
+        return new String(chars);
     }
 }
