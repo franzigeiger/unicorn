d9e581d057cda99739b5324c10afbe5e7b7aaf58
Tobias Beeh <tobias.beeh@t-online.de>
2018-01-24 15:14:56 +0100

    changed boxplot script and generated very nice looking boxplots

--- Dokumentation_DBS/jmeterResults/boxplots.py
+++ Dokumentation_DBS/jmeterResults/boxplots.py
@@ -6,11 +6,18 @@ import matplotlib.font_manager
 import csv
 import sys
 
-elapsed = {}
-timestamps = {}
-starttimes = {}
-errors = {}
+plot_size=(9,10)
+plot_ymin=100
+whisker_factor=3
+outlier_sym='.'
 
+def round_base(x, base=10):
+  f = 1
+  while f * base <= x:
+    f *= base
+  return f
+
+elapsed = {}
 # Parse the CSV files
 for file in sys.argv[1:]:
   threads = int(file.split('/')[-1].split('_')[0][:-1])
@@ -18,48 +25,30 @@ for file in sys.argv[1:]:
   for row in csv.DictReader(open(file)):
     if (not row['label'] in elapsed):
       elapsed[row['label']] = {}
-      timestamps[row['label']] = {}
-      starttimes[row['label']] = {}
-      errors[row['label']] = {}
-    if (not threads in elapsed[row['label']]):
+    elif (not threads in elapsed[row['label']]):
       elapsed[row['label']][threads] = []
-      timestamps[row['label']][threads] = []
-      starttimes[row['label']][threads] = []
-      errors[row['label']][threads] = []
-    elapsed[row['label']][threads].append(int(row['elapsed']))
-    timestamps[row['label']][threads].append(int(row['timeStamp']))
-    starttimes[row['label']][threads].append(int(row['timeStamp']) - int(row['elapsed']))
-    if (row['success'] != 'true'):
-      errors[row['label']][threads].append(int(row['elapsed']))
+    else:
+      elapsed[row['label']][threads].append(int(row['elapsed']))
 
 # Draw a separate figure for each label found in the results.
 for label in elapsed:
   # Transform the lists for plotting
   plot_data = []
-  throughput_data = [None]
-  error_x = []
-  error_y = []
   plot_labels = []
   column = 1
+  min_val = 1000
   for thread_count in sort(list(elapsed[label])):
+    #print(len(elapsed[label][thread_count]))
+    for val in elapsed[label][thread_count]:
+      if val < min_val:
+        min_val = val
     plot_data.append(elapsed[label][thread_count])
     plot_labels.append(thread_count)
-    test_start = min(starttimes[label][thread_count])
-    test_end = max(timestamps[label][thread_count])
-    test_length = (test_end - test_start) / 1000
-    num_requests = len(timestamps[label][thread_count]) - len(errors[label][thread_count])
-    if (test_length > 0):
-      throughput_data.append(num_requests / float(test_length))
-    else:
-      throughput_data.append(0)
-    for error in errors[label][thread_count]:
-      error_x.append(column)
-      error_y.append(error)
     column += 1
 
 
   # Start a new figure
-  fig = figure(figsize=(9, 6))
+  fig = figure(figsize=plot_size)
 
   # Pick some colors
   palegreen = matplotlib.colors.colorConverter.to_rgb('#8CFF6F')
@@ -68,13 +57,13 @@ for label in elapsed:
   # Plot response time
   ax1 = fig.add_subplot(111)
   ax1.set_yscale('log')
-  bp = boxplot(plot_data, notch=0, sym='+', vert=1, whis=1.5)
+  bp = boxplot(plot_data, notch=0, sym=outlier_sym, vert=1, whis=whisker_factor)
 
   # Tweak colors on the boxplot
   plt.setp(bp['boxes'], color='g')
   plt.setp(bp['whiskers'], color='g')
   plt.setp(bp['medians'], color='black')
-  plt.setp(bp['fliers'], color=palegreen, marker='+')
+  plt.setp(bp['fliers'], color='g', marker='.')
 
   # Now fill the boxes with desired colors
   numBoxes = len(plot_data)
@@ -89,28 +78,18 @@ for label in elapsed:
     boxPolygon = Polygon(boxCoords, facecolor=palegreen)
     ax1.add_patch(boxPolygon)
 
-  # Plot the errors
-  if (len(error_x) > 0):
-    ax1.scatter(error_x, error_y, color='r', marker='x', zorder=3)
-
-  # Plot throughput
-  ax2 = ax1.twinx()
-  ax2.plot(throughput_data, 'o-', color=paleblue, linewidth=2, markersize=8)
-
   # Label the axis
   ax1.set_title(label)
   ax1.set_xlabel('Number of concurrent requests')
-  ax2.set_ylabel('Requests per second')
   ax1.set_ylabel('Milliseconds')
-  ax1.set_xticks(range(1, len(plot_labels) + 1, 2))
-  ax1.set_xticklabels(plot_labels[0::2])
-  fig.subplots_adjust(top=0.9, bottom=0.15, right=0.85, left=0.15)
+  ax1.set_xticklabels(plot_labels)
+  #fig.subplots_adjust(top=0.9, bottom=0.15, right=0.85, left=0.15)
 
   # Turn off scientific notation for Y axis
   ax1.yaxis.set_major_formatter(ScalarFormatter(False))
 
   # Set the lower y limit to the match the first column
-  ax1.set_ylim(ymin=bp['boxes'][0].get_ydata()[0])
+  ax1.set_ylim(ymin=round_base(min_val, base=10))
 
   # Draw some tick lines
   ax1.yaxis.grid(True, linestyle='-', which='major', color='grey')
@@ -118,13 +97,5 @@ for label in elapsed:
   # Hide these grid behind plot objects
   ax1.set_axisbelow(True)
 
-  # Add a legend
-  line1 = Line2D([], [], marker='s', color=palegreen, markersize=10, linewidth=0)
-  line2 = Line2D([], [], marker='o', color=paleblue, markersize=8, linewidth=2)
-  line3 = Line2D([], [], marker='x', color='r', linewidth=0, markeredgewidth=2)
-  prop = matplotlib.font_manager.FontProperties(size='small')
-  figlegend((line1, line2, line3), ('Response Time', 'Throughput', 'Failures (50x)'),
-    'lower center', prop=prop, ncol=3)
-
   # Write the PNG file
-  savefig(label)
+  savefig(label + '.eps', format='eps')
